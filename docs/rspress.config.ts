16fa3071edd847185766aa85b2be4f9186ee6240
zhousg <zhousg1007@qq.com>
2023-11-14 19:05:55 +0800

    feat: docs

---
+++ docs/rspress.config.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'HarmonyOS Interview Handbook',
+  description: 'HarmonyOS Interview Handbook',
+  icon: "/rspress-icon.png",
+  logo: {
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
+  },
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+    ],
+  },
+});
