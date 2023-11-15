f809566e5069a7e71b5dced3452abc34449641ee
zhousg <zhousg1007@qq.com>
2023-11-15 23:34:21 +0800

    feat: docs

---
+++ docs/theme/index.tsx
@@ -0,0 +1,18 @@
+import Theme from "rspress/theme";
+
+const Layout = () => (
+  <Theme.Layout
+    afterFeatures={
+      <div className="overflow-hidden m-auto flex flex-wrap max-w-6xl" style={{ padding: 30 }}>
+        <img src="https://badges.toozhao.com/badges/01HF9APEFVN0K9ZG09DWQS6CJ3/blue.svg" alt="" />
+      </div>
+    }
+  />
+);
+
+export default {
+  ...Theme,
+  Layout,
+};
+
+export * from "rspress/theme";
